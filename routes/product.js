40078ad110833c01703c6f91de115ccd5c082d67
Rrr3rrr2004 <rabrobaaa@gmail.com>
2025-05-13 21:55:11 +0300

    add, update, list products

--- routes/product.js
+++ routes/product.js
@@ -10,28 +10,119 @@
 
 const express = require('express')
 const route = express.Router()
-const Item= require('../models/item')
+const Item = require('../models/item')
 
 //first lets handle the delete product logic function 
 
-route.delete('/:productName',async(req,res)=>{
-//and then now taking the paramter to can get the product id first 
-const productname=req.params.productName
-//there is a built in library in mongoose that help us to find the first element with this value in the db and delete it from there
+route.delete('/:productName', async (req, res) => {
+    //and then now taking the paramter to can get the product id first 
+    const productname = req.params.productName
+    //there is a built in library in mongoose that help us to find the first element with this value in the db and delete it from there
 
-if(!productname)//it means empty
-{return res.status(400).json({ message: "Product name is required." });}
+    if (!productname)//it means empty
+    { return res.status(400).json({ message: "Product name is required." }); }
 
-const findingtheproduct = await Item.findOneAndDelete({ name: productname });
+    const findingtheproduct = await Item.findOneAndDelete({ name: productname });
 
-if (!findingtheproduct) //it means this product name is not in the db
-{return res.status(404).json({ message: `Product with name "${productname}" not found.` });}
+    if (!findingtheproduct) //it means this product name is not in the db
+    { return res.status(404).json({ message: `Product with name "${productname}" not found.` }); }
 
-//otherwise so it is found and it is deleted successfully so return 200 as a response code
-res.status(200).json({ message: `Product "${productname}" deleted successfully.`});
+    //otherwise so it is found and it is deleted successfully so return 200 as a response code
+    res.status(200).json({ message: `Product "${productname}" deleted successfully.` });
 
 })
 
+route.post('/add', async (req, res) => {
+
+    const { name, price, description, image, calories, category } = req.body;
+
+    if (!req.user || req.user.role !== "admin") {
+        return res.status(403).json({ message: "Forbidden: Only admins can add products." });
+    }
+
+    try {
+        if (!name || !price || !description || !image || !calories || !category) { return res.status(400).json({ message: "Complete the product missing info" }); }
+
+
+        const find = await Item.findOne({ name: name });
+
+        if (find) {
+            return res.status(409).json({ message: "Product already added" });
+        }
+
+        const newItem = new Item({
+            name,
+            price,
+            description,
+            image,
+            calories,
+            category
+        })
+
+        await newItem.save();
+        res.status(201).json({ message: "product added successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: "error", error: error.message })
+    };
+
+
+});
+
+route.get('/list', async (req, res) => {
+
+    if (!req.user || req.user.role !== "admin") {
+        return res.status(403).json({ message: "Forbidden: Only admins can add products." });
+    }
+
+    try {
+        const products = await Item.find();
+
+        if (products.length == 0) {
+            return res.status(404).json({ message: "No products found" });
+        }
+
+        res.status(200).json({
+            message: "products found",
+            data: products
+
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "error", error: error.message });
+    }
+
+});
+
+route.put('/update/:productName', async (req, res) => {
+
+    try {
+        const productName = req.params.productName;
+        if (!productName)//it means empty
+        { return res.status(400).json({ message: "Product name is required." }); }
+
+        const product = await Item.findOneAndUpdate(
+            { name: productName },
+            req.body, // نادى البيانات اللي هتتعدل
+            { new: true } //to see after update
+        );
+
+        if (!product) {
+            return res.status(404).json({ message: `Product with name "${productName}" not found.` });
+        }
+
+        res.status(200).json({
+            message: `Product "${productName}" updated successfully.`,
+            product
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({ message: "error", error: error.message });
+    }
+
+
+
+
+});
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -11,25 +11,23 @@ router.get('/profile', auth, (req, res) => {
 
 // writing the code of delete user function 
 //tjis function will be auth to ensure the authorization and the admin is the only person who can delete the user 
-router.delete('/:userID',auth,async(req,res)=>{
+router.delete('/:userID', auth, async (req, res) => {
 
   if (!req.user || req.user.role !== "admin") { //that means it is a normal user want to make things to allowed to him
     return res.status(403).json({ message: "Forbidden: Only admins can delete users." });
-}
+  }
 
-const userid = req.params.userID;
-let numberuserid; 
-if (!userid) 
-{return res.status(400).json({ message: "User ID parameter is required." });}
 
-numberuserid = Number(userid);
-if (isNaN(numberuserid))
-{return res.status(400).json({ message: "User ID must be a valid number." });}
-const findinguser = await User.findOneAndDelete({ id: numberuserid });
-if (!findinguser)
- {return res.status(404).json({ message: `User with ID "${numberuserid}" not found.` });}
+  const userid = req.params.userID;
+  let numberuserid;
+  if (!userid) { return res.status(400).json({ message: "User ID parameter is required." }); }
 
-res.status(200).json({ message: `User with ID "${numberuserid}" deleted successfully.`});
+  numberuserid = Number(userid);
+  if (isNaN(numberuserid)) { return res.status(400).json({ message: "User ID must be a valid number." }); }
+  const findinguser = await User.findOneAndDelete({ id: numberuserid });
+  if (!findinguser) { return res.status(404).json({ message: `User with ID "${numberuserid}" not found.` }); }
+
+  res.status(200).json({ message: `User with ID "${numberuserid}" deleted successfully.` });
 
 })
 
