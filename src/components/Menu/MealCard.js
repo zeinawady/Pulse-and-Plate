7f7406a1de46e6ef26f05077d5aab8ee4763eaf2
HafsaTarek <161166356+HafsaTarek@users.noreply.github.com>
2025-05-15 00:20:13 +0300

    Update MealCard.js

--- src/components/Menu/MealCard.js
+++ src/components/Menu/MealCard.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './MealCard.css';
+import React, { useState } from "react";
+import "./MealCard.css";
 
 export default function MealCard({ meal }) {
   const [isFavorited, setIsFavorited] = useState(false);
@@ -11,21 +11,27 @@ export default function MealCard({ meal }) {
   return (
     <div className="meal-card">
       <div className="meal-image-container">
-        <img src={meal.image} alt={meal.name} className="meal-image" />
-        
+        {/* Meal image */}
+        <img src={meal.photo} alt={meal.name} className="meal-image" />
+
         {/* Overlay that shows on hover */}
         <div className="overlay">
           <button className="view-details">View Meal Details</button>
         </div>
 
         {/* Favorite Icon */}
-        <div className={`fav-icon ${isFavorited ? 'favorited' : ''}`} onClick={toggleFavorite}>
-          {isFavorited ? '❤️' : '🤍'}
+        <div
+          className={`fav-icon ${isFavorited ? "favorited" : ""}`}
+          onClick={toggleFavorite}
+        >
+          {isFavorited ? "❤️" : "🤍"}
         </div>
       </div>
 
       <h4 className="meal-name">{meal.name}</h4>
-      <h2 className="meal-price">{meal.price}</h2>
+      <h2 className="meal-price">
+        {meal.price} <span className="currency">EGP</span>
+      </h2>
       <button className="add-to-cart">Add to Cart</button>
     </div>
   );
