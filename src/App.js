6fb274110df5475eed4d076e196a41c94ea5a2af
Rrr3rrr2004 <rabrobaaa@gmail.com>
2025-05-15 11:35:15 +0300

    editing productinfo.css

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ import Header from './components/Header/Header';
 import About from './components/About/About';
 import Menu from './components/Menu/Menu';
 import ContactUs from './components/ContactUs/ContactUs';
+import ProductInfo from './components/ProductInfo/ProductInfo';
 
 function App() {
   return (
     <div>
       <Header />
       {/* <Home/> */}
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/contactus" element={<ContactUs />} />
-        
+        <Route path="/product-info" element={<ProductInfo />} />
+
       </Routes>
       <Footer />
     </div>
