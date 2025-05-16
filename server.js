1a2f4f69d8ba2ae3e311bea7def14f1f8f1066e9
Basmala Ayman <basmala.ayman.1904@gmail.com>
2025-05-17 02:41:50 +0300

    DOOOOONNNNEEEEEEE

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const MONGO_URI = process.env.MONGODB_URI;
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
